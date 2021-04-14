a582ff29497ae5605b1aafd59eb653b7e1ca9dbf
seosk526 <seosk526@gmail.com>
2021-04-14 17:43:38 +0900

    ChangeMoney_src

--- ChangeMoney_v2_kopo01강세영_210331/ChangeMoney/src/ChangeMoney/ConstValueClass.java
+++
@@ -1,8 +0,0 @@
-package ChangeMoney;
-
-public class ConstValueClass {
-	public static final int CHANGE_USD = 1;
-	public static final int CHANGE_EUR = 2;
-	public static final int CHANGE_JPY = 3;
-	public static final int EXIT = 0;	
-}
